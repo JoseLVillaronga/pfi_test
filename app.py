5f43c29a6655455fec06dec9f47bd5c924c070e2
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2024-09-30 22:03:46 -0300

    commit estructura inicial de PFI

---
+++ app.py
@@ -0,0 +1,15 @@
+#########################################
+# Voy a mostrar menu en pantalla...
+########################################
+print("Ingrese la opcion del menu correspondiente: \n")
+print("1- Mostrar la lista")
+print("2- Borrar la lista")
+print("3- Actualizar la lista")
+print("4- Crear la lista")
+
+opcion_seleccionada = int(input("Ingrese opcion: "))
+
+print("La opcion seleccionada fue: " , opcion_seleccionada)
+#########################################
+# Mostre menu en pantalla...
+########################################
